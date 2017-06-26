a19829036c1ca4fb1ea2e0bfd0d56bc26732fcef
Nishant <poddardelhi@gmail.com>
2017-06-27 01:51:33 +0300

    java script first ex completed

---
+++ First_JS_ex.js
@@ -0,0 +1,9 @@
+var inPound = prompt("Enter the value in pounds")
+var outKilo = inPound * 0.454
+alert("This  much  "+inPound+" pound is equal to" +outKilo )
+// alert("This is in Killogram" )
+console.log("Conversion Completed")
+// var lbs = prompt("What is the weight in pounds (lbs)?")
+// var kg = lbs*0.454
+// alert("That is:  "+kg+" kilograms")
+// console.log("Conversion Completed")
